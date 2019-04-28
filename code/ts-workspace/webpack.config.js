e228a205110d6f1c067b524ff8aed8def2516d6c
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-04-28 14:12:54 +0200

    ts workspace

---
+++ code/ts-workspace/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.ts?$/, use: ["ts-loader"], exclude: /node_modules/ }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
