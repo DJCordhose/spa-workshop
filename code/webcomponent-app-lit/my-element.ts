61ac627624b3c304ec8f5999c8a4dc49825333b2
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-05-17 14:03:43 +0200

    md wc rc1

---
+++ code/webcomponent-app-lit/my-element.ts
@@ -0,0 +1,23 @@
+import { LitElement, html, property, customElement } from 'lit-element'
+
+@customElement('my-element')
+class MyElement extends LitElement {
+    @property() greeting: string = 'Hello';
+
+    render() {
+        return html`
+        <style>
+        :host {
+            color: green;
+        }
+        </style>
+        <input value="${this.greeting}" @keyup="${(event: any) => this.greeting = event.target.value}">
+        <p>${this.greeting}, World</p>
+        <input type="button" value="Clear" @click="${this.handleClick}">
+        `
+    }
+
+    handleClick() {
+        this.greeting = '';
+    }
+}
