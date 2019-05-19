13ae1779abf4515d8b12b16b2ee33d864ed8cca8
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-05-19 14:26:04 +0200

    more complete lit-element example

--- code/webcomponent-app-lit/my-element.js
+++ code/webcomponent-app-lit/my-element.js
@@ -4,22 +4,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { LitElement, html, property, customElement } from 'lit-element';
+import { LitElement, html, css, property, customElement } from 'lit-element';
 let MyElement = class MyElement extends LitElement {
     constructor() {
         super(...arguments);
         this.greeting = 'Hello';
     }
-    render() {
-        return html `
-        <style>
+    // https://lit-element.polymer-project.org/guide/styles
+    static get styles() {
+        return css `
         :host {
+            all: initial;
             color: green;
         }
-        </style>
+        p {
+            background: var(--my-element-bg, #9E9E9E);
+        }`;
+    }
+    render() {
+        return html `
         <input value="${this.greeting}" @keyup="${(event) => this.greeting = event.target.value}">
+        <slot></slot>
         <p>${this.greeting}, World</p>
         <input type="button" value="Clear" @click="${this.handleClick}">
+        <slot name="after"></slot>
         `;
     }
     handleClick() {
--- code/webcomponent-app-lit/my-element.ts
+++ code/webcomponent-app-lit/my-element.ts
@@ -1,19 +1,28 @@
-import { LitElement, html, property, customElement } from 'lit-element'
+import { LitElement, html, css, property, customElement } from 'lit-element'
 
 @customElement('my-element')
 class MyElement extends LitElement {
     @property() greeting: string = 'Hello';
 
-    render() {
-        return html`
-        <style>
+    // https://lit-element.polymer-project.org/guide/styles
+    static get styles() {
+        return css`
         :host {
+            all: initial;
             color: green;
         }
-        </style>
+        p {
+            background: var(--my-element-bg, #9E9E9E);
+        }`;
+      } 
+
+    render() {
+        return html`
         <input value="${this.greeting}" @keyup="${(event: any) => this.greeting = event.target.value}">
+        <slot></slot>
         <p>${this.greeting}, World</p>
         <input type="button" value="Clear" @click="${this.handleClick}">
+        <slot name="after"></slot>
         `
     }
 
