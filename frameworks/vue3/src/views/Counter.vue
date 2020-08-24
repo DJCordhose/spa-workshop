560d1bd3f93aa71aa56f9a2101bc6c97fdb2817c
Oliver Zeigermann <oliver@zeigermann.de>
2020-08-24 14:45:09 +0200

    added counter playground

---
+++ frameworks/vue3/src/views/Counter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="counter">
+    <p>Counter: {{ counter }}</p>
+    <p>Times 10: {{ times10 }}</p>
+    <button v-on:click="subtract">Subtract 1</button>
+    <p><input v-model="counter" type="number" ></p>
+    <p>
+      Details can be found here
+      <a
+        href="https://v3.vuejs.org/guide/introduction.html"
+      >https://v3.vuejs.org/guide/introduction.html</a>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Counter",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000);
+  },
+  methods: {
+    subtract() {
+      this.counter--;
+    },
+  },
+  computed:  {
+    times10(): number {
+      return this.counter * 10;
+    },
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: blue;
+}
+</style>
